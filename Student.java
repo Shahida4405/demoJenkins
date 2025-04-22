b947d8391c37fd9551e12049223a31de763a8cb6
Ramya <ramyagabbur@gmail.com>
2025-04-22 14:01:06 +0530

    Add this file

---
+++ Student.java
@@ -0,0 +1,16 @@
+public class Student{
+    public static void main(String[] args) {
+        String Name="Shahida";
+        int id=073;
+        String clg="kletech";
+        String course="Devops";
+
+        System.out.println("-------Student Details--------");
+        System.out.println("student name:"+Name);
+        System.out.println("student id:"+id);
+        System.out.println("student clg:"+clg);
+        System.out.println("student course:"+course);
+        
+
+    }
+}
